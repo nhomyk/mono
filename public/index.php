0eda56e42f97588bfc6bf83f5c5c108f450f104a
“nicholashomyk” <nickhomyk@gmail.com>
2026-01-11 15:23:36 -0500

    12.1.11.2025
    fix CI

--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/Database.php';
 
 use App\Database;
 
--- scripts/init_db.php
+++ scripts/init_db.php
@@ -1,6 +1,5 @@
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/Database.php';
 
 use App\Database;
 
